8d441b140e15db1ec91bf7950ed00d09e47945f7
Dahye Kim <digeuthi@gmail.com>
2023-04-07 12:27:31 +0900

    website board start

---
+++ website/src/website/repository/BoardRepository.java
@@ -0,0 +1,17 @@
+package website.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import website.entity.Board;
+
+public class BoardRepository {
+
+	public static int index = 0;
+	private static List<Board> boardTable = new ArrayList<>();
+	
+	public Board save() {
+		
+	}
+	
+}
